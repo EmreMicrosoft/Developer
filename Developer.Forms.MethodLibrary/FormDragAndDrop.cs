56e1f0004746d7c65d3711d493b765e6e2622747
Emre Sevim <emre.xamarin@outlook.com>
2020-11-07 02:32:06 +0300

    ...

---
+++ Developer.Forms.MethodLibrary/FormDragAndDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Developer.Forms.MethodLibrary
+{
+    public partial class FormDragAndDrop : Form
+    {
+        public FormDragAndDrop()
+        {
+            InitializeComponent();
+        }
+
+        private bool _dragging;
+        private Point _dragCursorPoint;
+        private Point _dragFormPoint;
+
+        private void FormDragAndDrop_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;
+            _dragCursorPoint = Cursor.Position;
+            _dragFormPoint = Location;
+        }
+        private void FormDragAndDrop_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_dragging) return;
+            var dif = Point.Subtract(Cursor.Position, new Size(_dragCursorPoint));
+            Location = Point.Add(_dragFormPoint, new Size(dif));
+        }
+        private void FormDragAndDrop_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+    }
+}
