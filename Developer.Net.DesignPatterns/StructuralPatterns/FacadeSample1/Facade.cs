fad6e96b2de42fd8695b7fe21602656c0b81f36a
Emre Sevim <57841014+EmreMicrosoft@users.noreply.github.com>
2021-02-28 03:59:53 +0300

    Create Facade.cs

---
+++ Developer.Net.DesignPatterns/StructuralPatterns/FacadeSample1/Facade.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FacadePatternSample.SubSystems;
+
+namespace FacadePatternSample
+{
+    /// <summary>
+    /// The 'Facade' class
+    /// </summary>
+    internal class Facade
+    {
+        private readonly SubSystemOne _one;
+        private readonly SubSystemTwo _two;
+        private readonly SubSystemThree _three;
+        private readonly SubSystemFour _four;
+
+        public Facade()
+        {
+            _one = new SubSystemOne();
+            _two = new SubSystemTwo();
+            _three = new SubSystemThree();
+            _four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            _one.MethodTwo();
+            _one.MethodOne();
+            _two.MethodTwo();
+            _four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            _two.MethodTwo();
+            _two.MethodOne();
+            _three.MethodThree();
+        }
+    }
+}
