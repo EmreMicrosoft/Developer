f0682ff21cbdf776f8850877cce1e8af4bb884cc
Emre Sevim <57841014+EmreMicrosoft@users.noreply.github.com>
2021-02-28 04:04:32 +0300

    Create SubSystemThree.cs

---
+++ Developer.Net.DesignPatterns/StructuralPatterns/FacadeSample1/SubSystems/SubSystemThree.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacadePatternSample.SubSystems
+{
+    /// <summary>
+    /// The 'Subsystem ClassC' class
+    /// </summary>
+    internal class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine(" SubSystemThree Method3");
+        }
+    }
+}
