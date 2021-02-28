d76f7cf3348acc271d3eb1b9cd9ed789b98cae57
Emre Sevim <57841014+EmreMicrosoft@users.noreply.github.com>
2021-02-28 04:03:10 +0300

    Create SubSystemOne.cs

---
+++ Developer.Net.DesignPatterns/StructuralPatterns/FacadeSample1/SubSystems/SubSystemOne.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacadePatternSample.SubSystems
+{
+    /// <summary>
+    /// The 'Subsystem ClassA' class
+    /// </summary>
+    internal class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine(" SubSystemOne Method-S1M1");
+        }
+
+        public void MethodTwo()
+        {
+            Console.WriteLine(" SubSystemOne Method-S1M2");
+        }
+    }
+}
