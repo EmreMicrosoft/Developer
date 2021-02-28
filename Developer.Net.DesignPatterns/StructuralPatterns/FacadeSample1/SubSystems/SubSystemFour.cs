552bf5087241753319bb4e76c2dcbb94341b95c2
Emre Sevim <57841014+EmreMicrosoft@users.noreply.github.com>
2021-02-28 04:05:46 +0300

    Create SubSystemFour.cs

---
+++ Developer.Net.DesignPatterns/StructuralPatterns/FacadeSample1/SubSystems/SubSystemFour.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacadePatternSample.SubSystems
+{
+    /// <summary>
+    /// The 'Subsystem ClassD' class
+    /// </summary>
+    internal class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine(" SubSystemFour Method4");
+        }
+    }
+}
