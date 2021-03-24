9dd6e1f4f71f8450411dc4502db42864bad3dbf4
Emre Sevim <57841014+EmreMicrosoft@users.noreply.github.com>
2021-03-24 03:00:02 +0300

    Add files via upload

---
+++ Developer.Net.DesignPatterns/Singleton/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿namespace Developer.Net.CoreLibrary.DesignPatterns.Singleton
+{
+    public class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private static readonly object LockObject = new object();
+
+        private CustomerManager()
+        {
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            // Thread Safe
+            lock (LockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+
+            return _customerManager;
+        }
+
+        public void Save()
+        {
+            System.Console.WriteLine("Singleton is working");
+        }
+    }
+}
